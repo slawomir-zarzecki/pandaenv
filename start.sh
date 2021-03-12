7045a628526ac3bd3e721a48d1989839196d90b2
Sławek <sasek.mail@gmail.com>
2021-03-12 13:17:34 +0000

    init2

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+useradd -u 1030 artifactory
+chown -R artifactory:artifactory ./volumes/artifactory
+#useradd -u 1000 jenkins
+chown -R 1000:1000 ./volumes/jenkins
+cd compose
+docker-compose up -d --build
